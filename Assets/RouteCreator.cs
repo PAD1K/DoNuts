aa8f4c1004ea241b30c1221a4c901109119cce4c
Aramymo <maxoff9617@gmail.com>
2024-01-27 19:44:52 +0300

    Added Waypoints Generation
    Вэйпоинты теперь автоматически определяются в массив, также добавлена генерация вейпоинтов по овальной линии.

---
+++ Assets/RouteCreator.cs
@@ -0,0 +1,59 @@
+using System.Collections;
+using System.Collections.Generic;
+using Unity.VisualScripting;
+using UnityEditor;
+using UnityEngine;
+
+public class OvalRouteCreator : MonoBehaviour
+{
+    private List<Vector3> _ellipsePoints;
+    [SerializeField] private GameObject _waypointPrefab;
+    [SerializeField] private float _widthOfEllipse;
+    [SerializeField] private float _lengthOfEllipse;
+    [SerializeField] private int _numberOfPoints;
+    private GameObject _waypoint;
+    // Start is called before the first frame update
+    void Awake()
+    {
+        _ellipsePoints = GenerateEllipse(_widthOfEllipse,_lengthOfEllipse,_numberOfPoints);
+        for(int i = 0; i < _ellipsePoints.Count; i++)
+        {
+            CreateWaypoint(_ellipsePoints[i]);
+        }
+        //CreateWaypoint(new Vector3(0,0,0));
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    private void CreateWaypoint(Vector3 _waypointPosition)
+    {
+        _waypoint = Instantiate(_waypointPrefab);
+        _waypoint.transform.SetParent(this.transform);
+        _waypoint.transform.position = _waypointPosition;
+    }
+
+    private List<Vector3> GenerateEllipse(float _a, float _b, int _numberOfPoints)
+    {
+        Debug.Log("a=" + _a + " b=" + _b);
+        List<Vector3> points = new List<Vector3>();
+        // for(int i = 0; i < _number_of_points; i++)
+        // {
+        //     float t = (float)i / (float)_number_of_points * 2.0f * Mathf.PI;
+        //     points.Add(new Vector3(_a * Mathf.Cos(t), 0, _b * Mathf.Sin(t)));
+        // }
+        for (int i = 0; i <= _numberOfPoints; i++)
+        {
+            float angle = (float)i / (float)_numberOfPoints * 2f * Mathf.PI;
+            float x = Mathf.Cos(angle) * _a + (transform.position.x - _a);
+            float y = Mathf.Sin(angle) * _b + transform.position.z;
+            Debug.Log(i + ". " + "x=" + x + "; y=" + y);
+            //lineRenderer.SetPosition(i, new Vector3(x, y, 0f));
+            points.Add(new Vector3(x, 0, y));
+        }
+        return points;
+    }
+}
--- Assets/Scripts/Enemy/EnemyPatrol.cs
+++ Assets/Scripts/Enemy/EnemyPatrol.cs
@@ -1,18 +1,25 @@
 using System.Collections;
 using System.Collections.Generic;
+using Unity.VisualScripting;
 using UnityEditor;
 using UnityEngine;
 
 public class EnemyPatrol : MonoBehaviour
 {
-    [SerializeField] Transform[] _patrolPoints;
+    [SerializeField] List<Transform> _patrolPoints = new List<Transform>();
     [SerializeField] int _targetPoint;
     [SerializeField] float _speed;
+    [SerializeField] private  Transform _waypointsHolder;
+    private GameObject _enemy;
+    private GameObject _enemyPath;
     // Start is called before the first frame update
-    void Start()
+    void Awake()
     {
         _targetPoint = 0;
-
+        //FindPath();
+        //_patrolPoints = gameObject.GetComponentsInChildren<Transform>();
+        GetWayPoints();
+        
     }
 
     // Update is called once per frame
@@ -21,11 +28,20 @@ public class EnemyPatrol : MonoBehaviour
         if(transform.position == _patrolPoints[_targetPoint].position)
         {
             _targetPoint++;
-            if(_targetPoint >= _patrolPoints.Length)
+            if(_targetPoint >= _patrolPoints.Count)
             {
                 _targetPoint = 0;
             }
         }
         transform.position = Vector3.MoveTowards(transform.position, _patrolPoints[_targetPoint].position,_speed * Time.deltaTime);
+    }
+
+    void GetWayPoints()
+    {
+        _patrolPoints.Clear();
+        foreach(Transform child in _waypointsHolder)
+        {
+           _patrolPoints.Add(child);
+        }
     }
 }
