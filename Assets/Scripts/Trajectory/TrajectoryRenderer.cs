fc2b25c268fc2cfc8532d42f9f1ae306efb79313
Denis Tomarev <dentomarev@gmail.com>
2024-01-27 12:16:38 +0300

    [20] Рефактор кода

--- Assets/Scripts/Trajectory/TrajectoryRenderer.cs
+++ Assets/Scripts/Trajectory/TrajectoryRenderer.cs
@@ -6,19 +6,29 @@ using UnityEngine;
 public class TrajectoryRenderer : MonoBehaviour
 {
     [SerializeField] LineRenderer _lineRenderer;
+    [SerializeField] int _countOfDots = 100;
 
     public void ShowTrajectory(Vector3 origin, Vector3 velocity)
     {
-        Vector3[] points = new Vector3[100];
+        Vector3[] points = new Vector3[_countOfDots];
+        float time;
 
         _lineRenderer.positionCount = points.Length;
 
         for (int i = 0; i < points.Length; i++)
         {
-            float time = i * 0.1f;
+            time = i * Time.fixedDeltaTime;
+
+            //Если тело приземлилось, то прекращаем отрисовку.
+            if (Math.Abs(2 * velocity.y / Physics.gravity.y) <= time)
+            {
+                _lineRenderer.positionCount = i;
+                break;
+            }
+            
             points[i] = origin + velocity * time + Physics.gravity * time * time / 2;
         }
-        
+
         _lineRenderer.SetPositions(points);
     }
 }
