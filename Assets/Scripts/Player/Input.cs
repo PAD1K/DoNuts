789ee846a2d86629834f1ceb892734409919b15e
Denis Tomarev <dentomarev@gmail.com>
2024-01-27 21:02:49 +0300

    [5] Сделал стик для задания вектора прыжка

--- Assets/Scripts/Player/Input.cs
+++ Assets/Scripts/Player/Input.cs
@@ -1,3 +1,4 @@
+using System;
 using System.Collections;
 using System.Collections.Generic;
 using Unity.VisualScripting;
@@ -8,13 +9,26 @@ public class Controller : MonoBehaviour
     private PlayerInput _input;
     [SerializeField] MoveController _moveController;
     // Единичный вектор, задающий направление броска
-    [SerializeField] Vector3 _direction = new Vector3(1, 1, 0);
+    [SerializeField] Vector3 _direction = new Vector3(1, 1, 1);
+    [SerializeField] TrajectoryRenderer _trajectoryRenderer;
 
     void Awake()
     {
         _input = new PlayerInput();
         _input.Enable();
         _input.Player.Throw.performed += context => Throw();
+    }
+
+    private void Update()
+    {
+        SetDirection();
+    }
+
+    private void SetDirection()
+    {
+        Vector2 moveVector = _input.Player.Move.ReadValue<Vector2>();
+        _direction.x = moveVector.x;
+        _direction.z = moveVector.y;
     }
 
     private void Throw()
