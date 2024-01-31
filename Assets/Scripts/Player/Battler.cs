1649cc502592aac8058db5f62b74abc7d9cc4011
Denis Tomarev <dentomarev@gmail.com>
2024-02-01 00:03:10 +0300

    [64] Сделал таймер и событие проигрыша

--- Assets/Scripts/Player/Battler.cs
+++ Assets/Scripts/Player/Battler.cs
@@ -5,18 +5,44 @@ using UnityEngine;
 
 public class Battler : MonoBehaviour
 {
+    public delegate void LoseBarttleHandler ();
+    public static event LoseBarttleHandler OnLoseBattle;
     [SerializeField] private CameraZoomer _zoomer;
+    [SerializeField] private float _timeToLose = 30;
     private GameObject _player;
     private EnemyStats _stats;
-    
+    private float _startTime;
+    private bool _inBattle = false;
+
     private void Awake() 
     {
         _player = GameObject.FindGameObjectWithTag("Player");    
+        _startTime = 0;
+    }
+
+    private void Update() 
+    {
+        if (!_inBattle)
+        {
+            return;
+        }
+
+        if (_startTime == 0)
+        {
+            _startTime = Time.time;
+        }
+
+        if (Time.time > _startTime + _timeToLose)
+        {
+            _inBattle = false;
+            OnLoseBattle?.Invoke();
+        }    
     }
 
     private void StartBattle(EnemyStats stats)
     {
         _zoomer.Zoom();
+        _inBattle =true;
     }
 
     private void OnEnable() 
