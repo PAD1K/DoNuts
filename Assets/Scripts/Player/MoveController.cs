b148ef2058bd913c5424aba52ab5a32bf8be27c5
Denis Tomarev <dentomarev@gmail.com>
2024-02-03 23:56:39 +0300

    [81] Поменял логику камеры, реализовал обработку битвы и статы игрока, сделал стан

--- Assets/Scripts/Player/MoveController.cs
+++ Assets/Scripts/Player/MoveController.cs
@@ -5,9 +5,14 @@ public class MoveController : MonoBehaviour
     public delegate void LandHandler ();
     public static event LandHandler OnLandEvent;
     [SerializeField] private LayerMask _groundLayer;
-    [SerializeField] private float _height = 0.6f;
+    [SerializeField] private float _height = 0.6f; 
+    [SerializeField] private float _stickSensitivity = 0.5f;
+    [SerializeField] private float _stunTime = 5;
+    private Rigidbody _rigidbody;
     private bool _wasGrounded;
     private Vector3 _velocity;
+    private bool _isStun = false;
+    private float _startStunTime = 0;
 
     public bool IsPlayerGrounded
     {
@@ -24,11 +29,28 @@ public class MoveController : MonoBehaviour
     {
         return Physics.Raycast(transform.position, Vector3.down, _height, _groundLayer);
     }
+
+    public void Stun()
+    {
+        if (_isStun)
+        {
+            return;
+        }
+        
+        _isStun = true;
+        _startStunTime = Time.time;
+    }
+
     private void FixedUpdate()
     {
         if (IsGrounded() && !_wasGrounded)
         {
             OnLandEvent?.Invoke();
+        }
+
+        if (_isStun && Time.time > _startStunTime + _stunTime)
+        {
+            _isStun = false;
         }
         
         _wasGrounded = IsGrounded();
---
+++ Assets/Scripts/Player/PlayerStats.cs
@@ -0,0 +1,35 @@
+using System.Collections;
+using System.Collections.Generic;
+using TMPro.EditorUtilities;
+using UnityEngine;
+
+public class PlayerStats : MonoBehaviour
+{
+    [SerializeField] private uint _health;
+    private uint _points;
+    private MoveController _moveController;
+
+    public void IncreasePoints(uint increaseValue)
+    {
+        _points += increaseValue;
+    }
+    
+    public void TakeDamage(EnemyStats _enemyStats)
+    {
+        // TODO: получение урона от врага
+        // _health -= damage;
+        Stun();
+    }
+
+    public void Stun()
+    {
+        _moveController.Stun();
+    }
+
+    private void Awake() 
+    {
+        _points = 0;
+        CombatManager.OnLoseBattle += TakeDamage;
+        gameObject.TryGetComponent<MoveController>(out _moveController);
+    }
+}
